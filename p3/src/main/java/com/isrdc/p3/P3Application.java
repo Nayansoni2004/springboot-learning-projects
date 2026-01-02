67433471d937457c49e090bf31bf86ca941f6847
Nayansoni2004 <nayansoni142004@gmail.com>
2026-01-03 00:07:09 +0530

    This project demonstrates how Spring Boot automatically configures a servlet-based web application using dependencies, starts an embedded server, and manages the application lifecycle without manual configuration.

---
+++ p3/src/main/java/com/isrdc/p3/P3Application.java
@@ -0,0 +1,15 @@
+package com.isrdc.p3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P3Application.class, args);
+		
+		System.out.println("Hello Nobitaa...");
+	}
+
+}
