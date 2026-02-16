b718863527fada663a2bc5c3f0fefbdbf644f955
Nayansoni2004 <nayansoni142004@gmail.com>
2026-02-16 14:21:32 +0530

    Concept learned: form binding through use of Dto class! Spring MVC automatically binds form data to a DTO object using setter methods when field names match the object properties.

---
+++ web_mvc10/src/main/java/com/isrdc/WebMvc10Application.java
@@ -0,0 +1,13 @@
+package com.isrdc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebMvc10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebMvc10Application.class, args);
+	}
+
+}
